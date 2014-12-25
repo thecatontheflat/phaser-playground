ceeb09e3df96b068d7ab4e001236dcaa17142517
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-25 16:10:53 +0100

    Even more random walking

--- public/js/main.js
+++ public/js/main.js
@@ -4,19 +4,36 @@ Z.drawer = {
     x: 0,
     y: 0,
     draw: function () {
+        var ctx = this.ctx();
         this.clearCanvas();
-        var ctx = document.getElementById('canvas').getContext('2d');
 
         ctx.fillStyle = "rgb(200,0,0)";
-        ctx.fillRect(this.x, this.y, 55, 50);
+        ctx.fillRect(this.x, this.y, 50, 50);
 
         var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
-        this.x += Math.round(Math.random()) * plusOrMinus;
-        this.y += Math.round(Math.random()) * plusOrMinus;
+        //var plusOrMinus = 1;
+        var newX = Math.round(Math.random()) * plusOrMinus;
+        var newY = Math.round(Math.random()) * plusOrMinus;
+
+        if (newX < 0 && this.x - newX != 0) {
+            this.x += newX;
+        } else if (newX > 0) {
+            this.x += newX;
+        }
+
+        if (newY < 0 && this.y - newY != 0) {
+            this.y += newY;
+        } else if (newY > 0) {
+            this.y += newY;
+        }
     },
 
-    clearCanvas: function() {
-        var ctx = document.getElementById('canvas').getContext('2d');
+    clearCanvas: function () {
+        var ctx = this.ctx();
         ctx.clearRect(0, 0, 150, 150);
+    },
+
+    ctx: function () {
+        return document.getElementById('canvas').getContext('2d');
     }
 };
