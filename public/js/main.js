66b05d2effa4dc8dd0c114f2fce6f0a6d7d06df1
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-25 18:39:44 +0100

    Simple collision checks

--- public/js/main.js
+++ public/js/main.js
@@ -22,18 +22,18 @@ Z.walkingSquare = {
                 self.ctx.fillRect(self.x, self.y, self.size, self.size);
             },
 
-            redraw: function () {
+            recalculateCoordinates: function () {
                 var self = this;
 
                 var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
                 var newX = Math.round(Math.random()) * plusOrMinus;
                 var newY = Math.round(Math.random()) * plusOrMinus;
 
-                if (self.isLeftBorder(newX)) {
+                if (self.isLeftBorder(newX) && !self.isLeftCollision()) {
                     self.x += newX;
                 }
 
-                if (self.isRightBorder(newX)) {
+                if (self.isRightBorder(newX) && !self.isRightCollision()) {
                     self.x += newX;
                 }
 
@@ -44,8 +44,34 @@ Z.walkingSquare = {
                 if (self.isBottomBorder(newY)) {
                     self.y += newY;
                 }
+            },
 
+            //checkCollisions: function () {
+            //    var self = this;
+            //
+            //    var imgData = self.ctx.getImageData(self.x, self.y, 1, 1).data;
+            //    console.log('r', imgData[0]);
+            //    console.log('g', imgData[1]);
+            //    console.log('b', imgData[2]);
+            //},
+
+            redraw: function () {
+                var self = this;
                 self.draw();
+            },
+
+            isLeftCollision: function () {
+                var self = this;
+                var rgb = self.ctx.getImageData(self.x - 5, self.y, 1, 1).data;
+
+                return !(rgb[0] == 0 && rgb[1] == 0 && rgb[2] == 0);
+            },
+
+            isRightCollision: function () {
+                var self = this;
+                var rgb = self.ctx.getImageData(self.x + self.size + 5, self.y, 1, 1).data;
+
+                return !(rgb[0] == 0 && rgb[1] == 0 && rgb[2] == 0);
             },
 
             isLeftBorder: function (x) {
@@ -81,20 +107,27 @@ Z.drawer = {
 
     init: function () {
         this.initField();
-        var square1 = Z.walkingSquare.create(this.field, 50, 25);
-        //var square2 = Z.walkingSquare.create(this.field, 0, 0);
+        var square1 = Z.walkingSquare.create(this.field, 50, 50);
+        var square2 = Z.walkingSquare.create(this.field, 0, 0);
+        var square3 = Z.walkingSquare.create(this.field, 0, 100);
 
         square1.draw();
-        //square2.draw();
+        square2.draw();
+        square3.draw();
 
         this.squares.push(square1);
-        //this.squares.push(square2);
+        this.squares.push(square2);
+        this.squares.push(square3);
     },
 
     redraw: function () {
-        this.clearCanvas();
+        var i = 0;
+        for (i = 0; i < this.squares.length; i++) {
+            this.squares[i].recalculateCoordinates();
+        }
 
-        for (var i = 0; i < this.squares.length; i++) {
+        this.clearCanvas();
+        for (i = 0; i < this.squares.length; i++) {
             this.squares[i].redraw();
         }
     },
