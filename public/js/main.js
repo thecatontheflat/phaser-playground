609f415ee5b2c4517e1d50d6b18102a1952261c1
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-25 15:53:10 +0100

    Random walking

--- public/js/main.js
+++ public/js/main.js
@@ -4,13 +4,19 @@ Z.drawer = {
     x: 0,
     y: 0,
     draw: function () {
+        this.clearCanvas();
         var ctx = document.getElementById('canvas').getContext('2d');
-        ctx.clearRect(0, 0, 150, 150);
 
         ctx.fillStyle = "rgb(200,0,0)";
         ctx.fillRect(this.x, this.y, 55, 50);
 
-        this.x += 10;
-        this.y += 10;
+        var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        this.x += Math.round(Math.random()) * plusOrMinus;
+        this.y += Math.round(Math.random()) * plusOrMinus;
+    },
+
+    clearCanvas: function() {
+        var ctx = document.getElementById('canvas').getContext('2d');
+        ctx.clearRect(0, 0, 150, 150);
     }
 };
