3a7378e146cd3125d91a42132d3eae987fdbb735
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-26 15:05:24 +0100

    Added global time modificator

--- squares-game/public/js/main.js
+++ squares-game/public/js/main.js
@@ -13,10 +13,18 @@ Z.game = {
 
     loopCallback: function () {
         var self = this;
+        var now = Date.now();
+        var then = Date.now();
+        var delta = 0;
+
         return function () {
+            now = Date.now();
+            delta = (now - then) / 1000;
             //handleInput();
-            self.recalculateWorld();
+            self.recalculateWorld(delta);
             self.render();
+
+            then = now;
         };
     },
 
@@ -24,20 +32,20 @@ Z.game = {
         this.field.ctx.clearRect(0, 0, this.field.width, this.field.height);
     },
 
-    recalculateWorld: function () {
+    recalculateWorld: function (delta) {
         var player = this.objects[0];
 
         if (38 in this.keysDown) { // Player holding up
-            player.y -= player.speed * player.speed;
+            player.y -= player.speed * player.speed * delta;
         }
         if (40 in this.keysDown) { // Player holding down
-            player.y += player.speed * player.speed;
+            player.y += player.speed * player.speed * delta;
         }
         if (37 in this.keysDown) { // Player holding left
-            player.x -= player.speed * player.speed;
+            player.x -= player.speed * player.speed * delta;
         }
         if (39 in this.keysDown) { // Player holding right
-            player.x += player.speed * player.speed;
+            player.x += player.speed * player.speed * delta;
         }
     },
 
@@ -73,7 +81,7 @@ Z.player = {
     create: function (field) {
         var startX = 0;
         var startY = 0;
-        var speed = 1;
+        var speed = 10; //pixels per second
         var boxSize = 40;
         var ctx = field.ctx;
         var fillStyle = "rgb(200,0,0)";
