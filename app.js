4b1d51d21881aea61f0e4e7f3e9efbe9cd19a355
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-29 22:13:02 +0100

    Changes

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var sockets = require('./routes/sockets');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,16 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
 
 // development error handler
 // will print stacktrace
@@ -57,99 +42,12 @@ app.use(function (err, req, res, next) {
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
-
     var host = server.address().address;
     var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port)
-
+    console.log('App is listening at http://%s:%s', host, port)
 });
 
 var io = require('socket.io').listen(server);
-
-
-var counter = 0;
-var spawnObject = function () {
-    counter++;
-
-    var generateRandomColor = function () {
-        var color = [];
-        color.push(Math.round(Math.random() * 255));
-        color.push(Math.round(Math.random() * 255));
-        color.push(Math.round(Math.random() * 255));
-
-        return "rgb(" + color.join(',') + ")";
-    };
-
-    var getRandomInRange = function getRandomArbitrary (min, max) {
-        return Math.random() * (max - min) + min;
-    };
-
-    return {
-        id: counter,
-        x: 0, y: 0,
-        toX: 0, toY: 0,
-        size: getRandomInRange(20, 40),
-        fillStyle: generateRandomColor()
-    };
-};
-
-var Game = {
-    speed: 3,
-    objects: []
-};
-
-
-io.sockets.on('connection', function (client) {
-    var object = spawnObject();
-    client['object_id'] = object.id;
-    Game.objects[object.id] = object;
-
-    client.on('move', function (data) {
-        Game.objects[client['object_id']].toX = data.x;
-        Game.objects[client['object_id']].toY = data.y;
-    });
-
-    client.on('disconnect', function () {
-        var newObjects = [];
-        Game.objects.forEach(function (object) {
-            if (object.id != client['object_id']) {
-                newObjects[object.id] = object;
-            }
-        });
-
-        Game.objects = newObjects;
-    });
-});
-
-var serverLoop = function () {
-    var updateRequired = false;
-    Game.objects.forEach(function (object) {
-        if (object.x < object.toX) {
-            object.x += Game.speed;
-        }
-
-        if (object.x > object.toX) {
-            object.x -= Game.speed;
-        }
-
-        if (object.y < object.toY) {
-            object.y += Game.speed;
-        }
-
-        if (object.y > object.toY) {
-            object.y -= Game.speed;
-        }
-
-        updateRequired = true;
-    });
-
-    if (updateRequired) {
-        io.emit('render', Game.objects);
-    }
-};
-
-setInterval(serverLoop, 60);
-
+sockets(io);
 
 module.exports = app;
---
+++ routes/sockets.js
@@ -0,0 +1,84 @@
+module.exports = function (io) {
+    var counter = 0;
+    var spawnObject = function () {
+        counter++;
+
+        var generateRandomColor = function () {
+            var color = [];
+            color.push(Math.round(Math.random() * 255));
+            color.push(Math.round(Math.random() * 255));
+            color.push(Math.round(Math.random() * 255));
+
+            return "rgb(" + color.join(',') + ")";
+        };
+
+        var getRandomInRange = function getRandomArbitrary (min, max) {
+            return Math.random() * (max - min) + min;
+        };
+
+        return {
+            id: counter,
+            x: 0, y: 0,
+            toX: 0, toY: 0,
+            size: getRandomInRange(20, 40),
+            fillStyle: generateRandomColor()
+        };
+    };
+
+    var Game = {
+        speed: 3,
+        objects: []
+    };
+
+
+    io.sockets.on('connection', function (client) {
+        var object = spawnObject();
+        client['object_id'] = object.id;
+        Game.objects[object.id] = object;
+
+        client.on('move', function (data) {
+            Game.objects[client['object_id']].toX = data.x;
+            Game.objects[client['object_id']].toY = data.y;
+        });
+
+        client.on('disconnect', function () {
+            var newObjects = [];
+            Game.objects.forEach(function (object) {
+                if (object.id != client['object_id']) {
+                    newObjects[object.id] = object;
+                }
+            });
+
+            Game.objects = newObjects;
+        });
+    });
+
+    var serverLoop = function () {
+        var updateRequired = false;
+        Game.objects.forEach(function (object) {
+            if (object.x < object.toX) {
+                object.x += Game.speed;
+            }
+
+            if (object.x > object.toX) {
+                object.x -= Game.speed;
+            }
+
+            if (object.y < object.toY) {
+                object.y += Game.speed;
+            }
+
+            if (object.y > object.toY) {
+                object.y -= Game.speed;
+            }
+
+            updateRequired = true;
+        });
+
+        if (updateRequired) {
+            io.emit('render', Game.objects);
+        }
+    };
+
+    setInterval(serverLoop, 60);
+};
