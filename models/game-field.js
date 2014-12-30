aeae305a7fde84102e1798a5daa01e18f24dd66d
Vitaliy Zurian <vitaliy.zurian@wkda.de>
2014-12-30 16:43:52 +0100

    Collision logic

---
+++ models/game-field.js
@@ -0,0 +1,21 @@
+module.exports = {
+    objects: [],
+
+    checkCollision: function (currentObject, currentObjectNewX, currentObjectNewY) {
+        var collides = false;
+
+        this.objects.forEach(function (object) {
+            if (object.id != currentObject.id) {
+                var dx = (currentObjectNewX + currentObject.collisionRadius) - (object.x + object.collisionRadius);
+                var dy = (currentObjectNewY + currentObject.collisionRadius) - (object.y + object.collisionRadius);
+                var distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < currentObject.collisionRadius + object.collisionRadius) {
+                    collides = true;
+                }
+            }
+        });
+
+        return collides;
+    }
+};
