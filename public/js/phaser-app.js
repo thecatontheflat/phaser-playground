aad668f0154b305fb245d64f648889cb651c2ce1
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-01-04 19:16:16 +0100

    Walking skeleton

--- public/js/phaser-app.js
+++ public/js/phaser-app.js
@@ -7,139 +7,51 @@
     });
 
     function preload () {
-
-        game.load.atlas('tank', '/assets/tanks.png', '/assets/tanks.json');
-        game.load.atlas('enemy', '/assets/enemy-tanks.png', '/assets/tanks.json');
-        game.load.image('bullet', '/assets/bullet.png');
         game.load.image('earth', '/assets/scorched_earth.png');
-        game.load.spritesheet('kaboom', '/assets/explosion.png', 64, 64, 23);
-        game.load.image('skeleton', '/images/skeleton.png')
-
+        game.load.spritesheet('skeleton', '/images/skeleton_sprite.png', 64, 87);
     }
 
-    var land;
-
-    var shadow;
-    var tank;
-    var turret;
-
-    var enemies;
-    var enemyBullets;
-    var enemiesTotal = 0;
-    var enemiesAlive = 0;
-    var explosions;
-
-    var currentSpeed = 0;
     var cursors;
-
-    var bullets;
-    var fireRate = 100;
-    var nextFire = 0;
-
     var skeleton;
 
     function create () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        //  Resize our game world to be a 2000 x 2000 square
-        game.world.setBounds(-1000, -1000, 2000, 2000);
 
-        //  Our tiled scrolling background
-        land = game.add.tileSprite(0, 0, 800, 600, 'earth');
-        land.fixedToCamera = true;
-
-        skeleton = game.add.sprite(0,0,'skeleton', 'skeleton');
-        skeleton.scale.setTo(0.2, 0.2);
+        skeleton = game.add.sprite(0, 0, 'skeleton');
         game.physics.enable(skeleton, Phaser.Physics.ARCADE);
 
-        game.camera.follow(skeleton);
-        game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
-        game.camera.focusOnXY(0, 0);
+        skeleton.anchor.setTo(0.5, 0.5);
+        skeleton.animations.add('walk', [0, 1, 2]);
+        skeleton.body.collideWorldBounds = true;
 
         cursors = game.input.keyboard.createCursorKeys();
-
     }
 
     function update () {
-
         if (cursors.left.isDown) {
             skeleton.body.velocity.x = -300;
-        }  else if (cursors.right.isDown) {
+            skeleton.animations.play('walk', 10, true);
+            skeleton.scale.setTo(1, 1);
+        } else if (cursors.right.isDown) {
             skeleton.body.velocity.x = 300;
-        }  else if (cursors.up.isDown) {
+            skeleton.animations.play('walk', 10, true);
+            skeleton.scale.setTo(-1, 1);
+        } else if (cursors.up.isDown) {
             skeleton.body.velocity.y = -300;
-        }  else if (cursors.down.isDown) {
+            skeleton.animations.play('walk', 10, true);
+        } else if (cursors.down.isDown) {
             skeleton.body.velocity.y = 300;
+            skeleton.animations.play('walk', 10, true);
         } else {
             skeleton.body.velocity.y = 0;
             skeleton.body.velocity.x = 0;
-            //if (currentSpeed > 0) {
-            //    currentSpeed -= 4;
-            //}
+            skeleton.animations.stop('walk', true);
         }
-
-        if (currentSpeed > 0) {
-            //game.physics.arcade.velocityFromRotation(skeleton.rotation, currentSpeed, skeleton.body.velocity);
-        }
-
-        land.tilePosition.x = -game.camera.x;
-        land.tilePosition.y = -game.camera.y;
-
-        //  Position all the parts and align rotations
-        //shadow.x = tank.x;
-        //shadow.y = tank.y;
-        //shadow.rotation = tank.rotation;
-
-        //turret.x = tank.x;
-        //turret.y = tank.y;
-
-        //turret.rotation = game.physics.arcade.angleToPointer(turret);
-
-        if (game.input.activePointer.isDown) {
-            //  Boom!
-            //fire();
-        }
-
-    }
-
-    function bulletHitPlayer (tank, bullet) {
-
-        bullet.kill();
-
-    }
-
-    function bulletHitEnemy (tank, bullet) {
-
-        bullet.kill();
-
-        var destroyed = enemies[tank.name].damage();
-
-        if (destroyed) {
-            var explosionAnimation = explosions.getFirstExists(false);
-            explosionAnimation.reset(tank.x, tank.y);
-            explosionAnimation.play('kaboom', 30, false, true);
-        }
-
-    }
-
-    function fire () {
-
-        if (game.time.now > nextFire && bullets.countDead() > 0) {
-            nextFire = game.time.now + fireRate;
-
-            var bullet = bullets.getFirstExists(false);
-
-            bullet.reset(turret.x, turret.y);
-
-            bullet.rotation = game.physics.arcade.moveToPointer(bullet, 1000, game.input.activePointer, 500);
-        }
-
     }
 
     function render () {
-
         // game.debug.text('Active Bullets: ' + bullets.countLiving() + ' / ' + bullets.length, 32, 32);
         //game.debug.text('Enemies: ' + enemiesAlive + ' / ' + enemiesTotal, 32, 32);
-
     }
 
 })();
